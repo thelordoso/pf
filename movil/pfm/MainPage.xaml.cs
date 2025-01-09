5d6a77a18a45f168bce67b716f0b0af67c5e08f5
Armando Gonzalez Vazquez <ing.armandoglez@gmail.com>
2025-01-09 09:19:35 -0600

    Firsts microservice call

--- movil/pfm/MainPage.xaml.cs
+++ movil/pfm/MainPage.xaml.cs
@@ -9,16 +9,45 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	      var capacity = await GetCapacity();
+
+        if (count == 1)
+            CounterBtn.Text = $"Picó {count} vez la pata gorda. Capacidad: {capacity}%";
+        else
+            CounterBtn.Text = $"Picó {count} veces la pata gorda. Capacidad: {capacity}%";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+	private static async Task<int> GetCapacity()
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("Api_key", "6B9A27BB-CD5C-4004-8ACB-6323E8A619A9");
+
+        try
+        {
+            var response = await httpClient.GetAsync("https://v2.twinoaksadvantage.com/tosdapi/api/MemberInformation/GetCrowdLevel?clubId=3098");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var quota = (int)(decimal.Parse(responseBody) * 100);
+                return quota;
+            }
+
+            Console.WriteLine("Error en la llamada: " + response.StatusCode);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error en la llamada: " + ex.Message);
+        }
+
+        Console.WriteLine("No capacity info");
+        return 0;
+    }
 }
 
